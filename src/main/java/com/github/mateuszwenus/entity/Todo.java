63a79f571976a04c39ee76a3ecc11d822c4327c6
Mateusz Wenus <mateusz.wenus@gmail.com>
2023-01-24 21:59:47 +0100

    added flyway for database initialization

--- src/main/java/com/github/mateuszwenus/entity/Todo.java
+++ src/main/java/com/github/mateuszwenus/entity/Todo.java
@@ -1,13 +1,11 @@
 package com.github.mateuszwenus.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
 @Entity
+@Table(name = "TODOS")
 public class Todo {
 
     @Id
